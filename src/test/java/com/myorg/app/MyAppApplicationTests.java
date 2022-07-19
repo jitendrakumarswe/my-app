7d4b208249e9b6118bc7276e539cc28683091e25
Lenovo <Lenovo@DESKTOP-IRNU16R.mshome.net>
2022-07-20 01:35:08 +0530

    initial commit

---
+++ src/test/java/com/myorg/app/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
